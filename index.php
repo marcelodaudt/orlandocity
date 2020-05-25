169c706589f3f00f436f13e13b4c78544eca2bc1
Marcelo Daudt <mdaudt@yahoo.com.br>
2020-05-25 20:26:40 -0300

    Implementado Angular.js e criado REST API com Rotas.

--- index.php
+++ index.php
@@ -35,4 +35,26 @@ $app->get(
     }
 );
 
+$app->get(
+    '/produtos',
+    function (){
+
+        $sql = new Sql();
+
+        $data = $sql->select("SELECT * FROM tb_produtos WHERE preco_promorcional > 0 ORDER BY preco_promorcional DESC LIMIT 3;");
+
+        foreach ($data as &$produto) {
+            $preco = $produto['preco'];
+            $centavos = explode(".", $produto['preco']);
+            $produto['preco'] = number_format($preco, 0, ",", ".");
+            $produto['centavos'] = end($centavos);
+            $produto['parcelas'] = 10;
+            $produto['parcela']  = number_format($preco/$produto['parcelas'], 2, ",", ".");
+            $produto['total'] = number_format($preco, 2, ",", ".");
+        }
+
+        echo json_encode($data);
+
+});
+
 $app->run();
--- view/shop.php
+++ view/shop.php
@@ -2,52 +2,28 @@
 
 <section>
 
-	<div class="container" id="destaque-produtos-container">
+	<div class="container" id="destaque-produtos-container" ng-controller="destaque-controller">
 		
 		<div id="destaque-produtos">
 			
-			<div class="item">
+			<div class="item" ng-repeat="produto in produtos">
 				
 				<div class="col-sm-6 col-imagem">
 
-					<img src="img/produtos/moto-x.png" alt="Nome do Produto">
+					<img src="img/produtos/{{produto.foto_principal}}" alt="{{produto.nome_prod_longo}}">
 
 				</div>
 				
 				<div class="col-sm-6 col-descricao">
-					<h2>Smartphone Motorola Moto X Play Dual Chip Desbloqueado Android 5.1</h2>
+					<h2>{{produto.nome_prod_longo}}</h2>
 					<div class="box-valor">
 						<div class="text-noboleto text-arial-cinza">no boleto</div>
 						<div class="text-por text-arial-cinza">por</div>
 						<div class="text-reais text-roxo">R$</div>
-						<div class="text-valor text-roxo">1.259</div>
-						<div class="text-valor-centavos text-arial-cinza">,10</div>
-						<div class="text-parcelas text-arial-cinza">ou em até 8x de R$ 174,88</div>
-						<div class="text-total">total a prazo R$ 1.399,00</div>
-					</div>
-					<a href="#" class="btn btn-comprar text-roxo"><i class="fa fa-shopping-cart"></i> compre agora</a>
-				</div>
-
-			</div>
-
-			<div class="item">
-				
-				<div class="col-sm-6 col-imagem">
-
-					<img src="img/produtos/moto-x.png" alt="Nome do Produto">
-
-				</div>
-				
-				<div class="col-sm-6 col-descricao">
-					<h2>Smartphone Motorola Moto X Play Dual Chip Desbloqueado Android 5.1</h2>
-					<div class="box-valor">
-						<div class="text-noboleto text-arial-cinza">no boleto</div>
-						<div class="text-por text-arial-cinza">por</div>
-						<div class="text-reais text-roxo">R$</div>
-						<div class="text-valor text-roxo">1.259</div>
-						<div class="text-valor-centavos text-arial-cinza">,10</div>
-						<div class="text-parcelas text-arial-cinza">ou em até 8x de R$ 174,88</div>
-						<div class="text-total">total a prazo R$ 1.399,00</div>
+						<div class="text-valor text-roxo">{{produto.preco}}</div>
+						<div class="text-valor-centavos text-arial-cinza">,{{produto.centavos}}</div>
+						<div class="text-parcelas text-arial-cinza">ou em até {{produto.parcelas}}x de R$ {{produto.parcela}}</div>
+						<div class="text-total">total a prazo R$ {{produto.total}}</div>
 					</div>
 					<a href="#" class="btn btn-comprar text-roxo"><i class="fa fa-shopping-cart"></i> compre agora</a>
 				</div>
@@ -206,28 +182,53 @@
 <?php include_once("footer.php"); ?>
 
 <script>
+
+	angular.module("shop", []).controller("destaque-controller", function($scope, $http){
+
+		$scope.produtos = [];
+
+		var initCarousel = function(){
+
+			$('#destaque-produtos').owlCarousel({
+
+				autoPlay : 5000,
+				items : 4,
+				singleItem : true
+			
+			});
+
+			var owlDestaque = $('#destaque-produtos').data('owlCarousel');
+
+			$('#btn-destaque-prev').on('click', function(){
+
+				owlDestaque.prev();
+
+			});
+
+			$('#btn-destaque-next').on('click', function(){
+
+				owlDestaque.next();
+
+			});
+		
+		};
+
+		$http({
+			method: 'GET',
+			url: 'produtos'
+		}).then(function successCallback(response){
+			
+			$scope.produtos = response.data;
+
+			setTimeout(initCarousel, 1000);
+
+		}, function errorCallback(response){
+
+		});
+
+	});
+
 	$(function(){
-
-		$('#destaque-produtos').owlCarousel({
-
-			autoPlay : 5000,
-			items : 4,
-			singleItem : true
-		});
-
-		var owlDestaque = $('#destaque-produtos').data('owlCarousel');
-
-		$('#btn-destaque-prev').on('click', function(){
-
-			owlDestaque.prev();
-
-		});
-
-		$('#btn-destaque-next').on('click', function(){
-
-			owlDestaque.next();
-
-		});
 
 		$('.estrelas').each(function(){
 
