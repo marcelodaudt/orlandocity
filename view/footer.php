6b269711bb2425d9fe7cfddfbfb1fa158178cb59
Marcelo Daudt <mdaudt@yahoo.com.br>
2020-05-25 18:26:43 -0300

    Finalizada a criação da Home da Loja.

--- view/footer.php
+++ view/footer.php
@@ -100,6 +100,7 @@
 	<script src="lib/jquery/jquery.min.js"></script>
 	<script src="lib/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
 	<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+	<script src="lib/raty/lib/jquery.raty.js"></script>
 	<script src="js/efeitos.js"></script>
 
 </body>
--- view/header.php
+++ view/header.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="lib/owl.carousel/owl-carousel/owl.carousel.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="lib/raty/lib/jquery.raty.css">
 	<link rel="stylesheet" href="css/orlando.css">
 	<link rel="stylesheet" href="css/orlando-mobile.css">
 
