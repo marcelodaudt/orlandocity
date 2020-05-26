9812d5e1254c956bcd653e8b89f92c9cff0ec735
Marcelo Daudt <mdaudt@yahoo.com.br>
2020-05-26 20:21:00 -0300

    Criada Lista de Mais Buscados na Loja com Back-End.

--- view/shop.php
+++ view/shop.php
@@ -1,8 +1,8 @@
 <?php include_once("header.php"); ?>
 
-<section>
+<section ng-controller="destaque-controller">
 
-	<div class="container" id="destaque-produtos-container" ng-controller="destaque-controller">
+	<div class="container" id="destaque-produtos-container">
 		
 		<div id="destaque-produtos">
 			
@@ -97,75 +97,18 @@
 
 		<div class="row">
 			
-			<div class="col-md-3">
+			<div class="col-md-3" ng-repeat="produto in buscados">
 
 				<div class="box-produto-info">
 				
 					<a href="#">
 					
-						<img src="img/produtos/panelas.png" alt="Panelas" class="produto-img">
-						<h3>Conjunto de Panelas Tramontina Versalhes Alumínio Antiaderente 5</h3>
-						<div class="estrelas" data-score="3"></div>
-						<div class="text-qtd-reviews text-arial-cinza">(300)</div>
-						<div class="text-valor text-roxo">R$ 109,90</div>
-						<div class="text-parcelado text-arial-cinza">10x de R$ 10,99 sem juros</div>
-					
-					</a>
-
-				</div>
-			
-			</div>
-
-						<div class="col-md-3">
-
-				<div class="box-produto-info">
-				
-					<a href="#">
-					
-						<img src="img/produtos/panelas.png" alt="Panelas" class="produto-img">
-						<h3>Conjunto de Panelas Tramontina Versalhes Alumínio Antiaderente 5</h3>
-						<div class="estrelas" data-score="5"></div>
-						<div class="text-qtd-reviews text-arial-cinza">(300)</div>
-						<div class="text-valor text-roxo">R$ 109,90</div>
-						<div class="text-parcelado text-arial-cinza">10x de R$ 10,99 sem juros</div>
-					
-					</a>
-
-				</div>
-			
-			</div>
-
-						<div class="col-md-3">
-
-				<div class="box-produto-info">
-				
-					<a href="#">
-					
-						<img src="img/produtos/panelas.png" alt="Panelas" class="produto-img">
-						<h3>Conjunto de Panelas Tramontina Versalhes Alumínio Antiaderente 5</h3>
-						<div class="estrelas" data-score="2.5"></div>
-						<div class="text-qtd-reviews text-arial-cinza">(300)</div>
-						<div class="text-valor text-roxo">R$ 109,90</div>
-						<div class="text-parcelado text-arial-cinza">10x de R$ 10,99 sem juros</div>
-					
-					</a>
-
-				</div>
-			
-			</div>
-
-						<div class="col-md-3">
-
-				<div class="box-produto-info">
-				
-					<a href="#">
-					
-						<img src="img/produtos/panelas.png" alt="Panelas" class="produto-img">
-						<h3>Conjunto de Panelas Tramontina Versalhes Alumínio Antiaderente 5</h3>
-						<div class="estrelas" data-score="4"></div>
-						<div class="text-qtd-reviews text-arial-cinza">(300)</div>
-						<div class="text-valor text-roxo">R$ 109,90</div>
-						<div class="text-parcelado text-arial-cinza">10x de R$ 10,99 sem juros</div>
+						<img src="img/produtos/{{produto.foto_principal}}" alt="{{produto.nome_prod_longo}}" class="produto-img">
+						<h3>{{produto.nome_prod_longo}}</h3>
+						<div class="estrelas" data-score="{{produto.media}}"></div>
+						<div class="text-qtd-reviews text-arial-cinza">({{produto.total_reviews}})</div>
+						<div class="text-valor text-roxo">R$ {{produto.total}}</div>
+						<div class="text-parcelado text-arial-cinza">{{produto.parcelas}}x de R$ {{produto.parcela}} sem juros</div>
 					
 					</a>
 
@@ -226,22 +169,37 @@
 
 		});
 
-	});
+		var initEstrelas = function(){
 
-	$(function(){
+			$('.estrelas').each(function(){
 
-		$('.estrelas').each(function(){
+				$(this).raty({
+				
+					starHalf : 'lib/raty/lib/images/star-half.png',
+					starOff	: 'lib/raty/lib/images/star-off.png',
+					starOn	: 'lib/raty/lib/images/star-on.png',
+					score : parseFloat($(this).data("score"))
+				
+				});
 
-			$(this).raty({
-			
-				starHalf : 'lib/raty/lib/images/star-half.png',
-				starOff	: 'lib/raty/lib/images/star-off.png',
-				starOn	: 'lib/raty/lib/images/star-on.png',
-				score : parseFloat($(this).data("score"))
-			
 			});
 
-		});
+		};
+
+		$http({
+			method: 'GET',
+			url: 'produtos-mais-buscados'
+		}).then(function successCallback(response) {
+
+			$scope.buscados = response.data;
+
+			setTimeout(initEstrelas, 1000);
+
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
 
 	});
+
 </script>
